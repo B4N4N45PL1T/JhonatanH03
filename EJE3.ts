bf19a9f9761e819be54736af69dd17a5a0ea919a
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-09 09:23:17 -0400

    //Ejercicio 3

---
+++ EJE3.ts
@@ -0,0 +1,13 @@
+class Tarea {
+    constructor(public titulo: string, public completa: boolean) {}
+  }
+  const listaDeTareas: Tarea[] = [
+    new Tarea("Ir a la Universidad", false),
+    new Tarea("Sacar la basura", true),
+    new Tarea("Estudiar TypeScript", false),
+  ];
+  console.log("Lista de tareas:");
+  for (const tarea of listaDeTareas) {
+    const estado = tarea.completa ? "Completa" : "Pendiente";
+    console.log(` ${tarea.titulo} - ${estado}`);
+  }

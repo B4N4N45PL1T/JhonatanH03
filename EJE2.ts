49e8f4e483fe946fae9132b0347675df9acf9aab
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-09 09:23:03 -0400

    //Ejercicio 2

---
+++ EJE2.ts
@@ -0,0 +1,12 @@
+function esParOImpar(numero: number): string {
+    if (numero % 2 === 0) {
+      return "Par";
+    } else {
+      return "Impar";
+    }
+  }
+  
+  const numero = 5;
+  const Rtado = esParOImpar(numero);
+  
+  console.log(`El número ${numero} es ${Rtado}`);

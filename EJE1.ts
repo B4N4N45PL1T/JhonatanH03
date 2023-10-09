6d145c0f6fbe69139b55ff588270e2486f2fc6d2
JhonatanH03 <jhonatanhernandezjorge2016@gmail.com>
2023-10-09 09:22:46 -0400

    //Ejercicio 1

---
+++ EJE1.ts
@@ -0,0 +1,10 @@
+function sumaNumeros(NE1: number, NE2: number): number {
+    const suma = NE1 + NE2;
+    return suma;
+  }
+  
+  const numero1 = 5;
+  const numero2 = 3;
+  const resultado = sumaNumeros(numero1, numero2);
+  
+  console.log(`La suma de ${numero1} y ${numero2} es ${resultado}`);

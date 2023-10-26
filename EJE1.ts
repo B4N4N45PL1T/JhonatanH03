dcbf0e5f44896d58832b13cc4ee7a3cd2d199d2c
JhonatanH03 <147352690+JhonatanH03@users.noreply.github.com>
2023-10-25 22:17:25 -0400

    Update EJE1.ts

--- EJE1.ts
+++ EJE1.ts
@@ -8,3 +8,5 @@ function sumaNumeros(NE1: number, NE2: number): number {
   const resultado = sumaNumeros(numero1, numero2);
   
   console.log(`La suma de ${numero1} y ${numero2} es ${resultado}`);
+
+//Hola mundo
